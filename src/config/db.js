ea10a87139fc58ae67add34c3a0d768a096adc93
Khushhal Sharma <100305608+KhushhalSharma@users.noreply.github.com>
2022-12-23 12:43:36 +0530

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://khushhal:dk35403540@cluster0.k6a7pjj.mongodb.net/b19"
+    "mongodb+srv://<name>:<password>@cluster0.k6a7pjj.mongodb.net/b19"
   );
 };
 
